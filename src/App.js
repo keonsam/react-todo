2e0bdba5076ccd54c11c6ddc7dec7562054f6931
keonsam <keonsamuel95@gmail.com>
2019-07-18 08:07:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { TodoRow } from "./TodoRow";
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { VisibilityControl } from "./VisibilityControl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Keon',
+      todoItems: [{ action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }],
+      showCompleted: true
+    }
+  }
+
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null
+        ? JSON.parse(data)
+        : {
+          userName: "Adam",
+          todoItems: [{ action: "Buy Flowers", done: false },
+            { action: "Get Shoes", done: false },
+            { action: "Collect Tickets", done: true },
+            { action: "Call Joe", done: false }],
+          showCompleted: true
+        });
+  };
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  };
+
+  setStorage() {
+    localStorage.setItem("todos", JSON.stringify(this.state));
+  }
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)) {
+      this.setState({
+        todoItems: [...this.state.todoItems, { action: task, done: false }]
+      }, () => this.setStorage() );
+    }
+  };
+
+  toggleTodo = (todo) => {
+    this.setState({ todoItems:
+          this.state.todoItems.map(item => item.action === todo.action
+              ? { ...item, done: !item.done } : item) }, () => this.setStorage());
+  };
+
+  todoTableRows = (doneValue) => {
+    return this.state.todoItems.filter(item => item.done === doneValue).map(item => <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />);
+  };
+
+  render() {
+    return (
+        <div>
+          <TodoBanner name={ this.state.userName } tasks={this.state.todoItems } />
+          <div className="container-fluid">
+            <TodoCreator callback={ this.createNewTodo } />
+            <table className="table table-striped table-bordered">
+              <thead>
+              <tr><th>Description</th><th>Done</th></tr>
+              </thead>
+              <tbody>{ this.todoTableRows(false) }</tbody>
+            </table>
+            <div className="bg-secondary text-white text-center p-2">
+              <VisibilityControl description="Completed Tasks"
+                                 isChecked={this.state.showCompleted}
+                                 callback={ (checked) =>
+                                     this.setState({ showCompleted: checked })} />
+            </div>
+            { this.state.showCompleted &&
+            <table className="table table-striped table-bordered">
+              <thead>
+              <tr><th>Description</th><th>Done</th></tr>
+              </thead>
+              <tbody>{ this.todoTableRows(true) }</tbody>
+            </table>
+            }
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
